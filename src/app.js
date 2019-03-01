d267a986af7f212f6f8ba0015103f86317f4db74
Christopher <christopher.carnivale@gmail.com>
2019-03-01 09:24:39 -0500

    articles router needs to be added

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
-const ArticlesService = require('./articles-service'); 
+const ArticlesService = require('./articles-service');
+const usersRouter = require('./users/users-router'); 
+const commentsRouter = require('./comments/comments-router');
 
 const app = express();
 
@@ -17,6 +19,8 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+app.use('/api/users', usersRouter);
+app.use('/api/comments', commentsRouter); 
 
 app.get('/articles', (req, res, next) =>{
   const knexInstance = req.app.get('db');
@@ -44,6 +48,31 @@ app.get('/articles/:article_id', (req, res, next) => {
 app.get('/', (req, res) => {
   res.send('Hello, boilerplate!');
 });
+
+app.patch('/articles/:article_id', jsonParser, (req, res, next) => {
+  const { title, content, style } = req.body;
+  const articleToUpdate = { title, content, style };
+
+  const numberOfValues = Object.values(articleToUpdate).filter(Boolean).length;
+  if (numberOfValues === 0) {
+    return res.status(400).json({
+      error: {
+        message: 'Request body must content either \'title\', \'style\' or \'content\''
+      }
+    });
+  }
+
+  ArticlesService.updateArticle(
+    req.app.get('db'),
+    req.params.article_id,
+    articleToUpdate
+  )
+    .then(numRowsAffected => {
+      res.status(204).end();
+    })
+    .catch(next);
+});
+
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
